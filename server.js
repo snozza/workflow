899083230573e464cf0c7675742bdfa416ca72c0
snozza <andrewcsnead@gmail.com>
2016-08-20 09:39:44 +0100

    change method to look up costs by individual job

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').load({ silent: true });
 const Express = require('express');
 const fetch = require('node-fetch');
 const co = require('co');
+const json2csv = require('json2csv');
 const { parseString } = require('xml2js');
 const config = require('./config');
 const app = new Express();
@@ -24,8 +25,17 @@ function parseXML(rawText) {
 }
 
 function* getJobs() {
-  const path = '/job.api/current';
-  const query = `?apiKey=${config.apiKey}&accountKey=${config.accountKey}`;
+  const path = `/job.api/current/`
+  const query = `?apiKey=${config.apiKey}&accountKey=${config.accountKey}&detailed=true`;
+  const response = yield fetch(config.workflowHost + path + query, options);
+  const rawText = yield response.text();
+  const resXML = yield parseXML(rawText);
+  return resXML.Response.Jobs[0].Job;
+}
+
+function* getJobs() {
+  const path = `/job.api/current/`
+  const query = `?apiKey=${config.apiKey}&accountKey=${config.accountKey}&detailed=true`;
   const response = yield fetch(config.workflowHost + path + query, options);
   const rawText = yield response.text();
   const resXML = yield parseXML(rawText);
@@ -41,30 +51,45 @@ function* getInvoices() {
   return resXML.Response.Invoices[0].Invoice;
 }
 
+function* getCostsByJob(jobId) {
+  const path = `/job.api/costs/${jobId}`
+  const query = `?apiKey=${config.apiKey}&accountKey=${config.accountKey}&detailed=true`;
+  const response = yield fetch(config.workflowHost + path + query, options);
+  const rawText = yield response.text();
+  const resXML = yield parseXML(rawText);
+  return resXML.Response.Costs[0];
+}
+
 app.get('/', (req, res) => {
   const csvRows = [];
   co(function* () {
+    const jobs = yield getJobs();
     const invoices = yield getInvoices();
-    if (invoices) {
-      yield invoices.map(invoice => {
-        const job = invoice.Jobs[0].Job[0]; 
-        let actualCosts = 0; 
-        job.Costs[0].Cost.map(cost => {
-          actualCosts += Number(cost.AmountIncludingTax[0]); 
-        });
-        const invoicedAmount = Number(invoice.AmountIncludingTax[0]); 
-        const profit = invoicedAmount - actualCosts;
-        const row = { 
-          'Name': job.Name[0],
-          'Invoiced': invoicedAmount,
-          'Actual Costs': actualCosts,
-          Profit: profit
+    if (jobs) {
+      yield jobs.map(function* (job) {
+        const row = {
+          Name: job.Name[0],
+          Invoiced: 0,
+          'Actual Costs': 0,
+          Profit: 0
         };
+        const costs = yield getCostsByJob(job.ID[0]);
+        if (costs) {
+          costs.Cost.forEach(cost => {
+            row['Actual Costs'] += Number(cost.Quantity) * Number(cost.UnitCost);
+          });
+          const jobInvoice = invoices.find(invoice => invoice.JobText[0] === job.ID[0]);
+          if (jobInvoice) {
+            row.Invoiced = Number(jobInvoice.AmountIncludingTax);
+          }
+          row.Profit = row.Invoiced - row['Actual Costs'];
+        }
         csvRows.push(row);
       });
-      console.log(csvRows);
+      const csvOutput = json2csv({ data: csvRows });
+      res.attachment('totals.csv');
+      return res.send(csvOutput);
     }
-        
     res.send('OK');
   }).catch(err => {
     console.log(err);
